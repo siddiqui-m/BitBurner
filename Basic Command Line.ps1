83293ddd19b75cda3c6e29ef3fad72d17fc3a535
siddiqui-m <muhammad.siddiqui98@hotmail.com>
2026-09-14 00:20:53 +0500

    Basic commands

---
+++ Basic Command Line.ps1
@@ -0,0 +1,9 @@
+# Bitburner uses Netscript (.script) and JavaScript (.js) for its terminal commands
+
+# Here are some basic commands you can use.
+
+ls 
+# Lists all the files in the current directory
+
+run <script>
+#Runs the script in the server.
